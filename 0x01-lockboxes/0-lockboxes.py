7e219fdea82942f3cff1bcc7aa6f511fc34a5774
vantwill <vtwill41@gmail>
2023-02-02 19:30:04 +0300

    commit

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,11 +4,11 @@
 
 def canUnlockAll(boxes):
     """ returns a bool showing can or cannot unlock box """
-    boxes_dict = {}
+    bxes_dict = {}
     for box in range(len(boxes)):
-        boxes_dict[box] = False
+        bxes_dict[box] = False
 
-    boxes_dict[0] = True
+    bxes_dict[0] = True
 
     # keys_set = set([key for keys in boxes for key in keys])
     keys_set = set()
@@ -19,20 +19,20 @@ def canUnlockAll(boxes):
 
     init_len_of_keys = len(keys_set)  # used to check when no new keys found
     while True:
-        for lock in boxes_dict:
-            if not boxes_dict[lock]:
+        for lock in bxes_dict:
+            if not bxes_dict[lock]:
                 if lock in keys_set:
-                    boxes_dict[lock] = True
+                    bxes_dict[lock] = True
                     keys_set.update(boxes[lock])
         if len(keys_set) == init_len_of_keys:  # no new keys: break
             break
         else:
             init_len_of_keys = len(keys_set)
     # print(boxes_dict, keys_set)
-    return check_boxes_dict(boxes_dict)
+    return check_bxes_dict(bxes_dict)
 
 
-def check_boxes_dict(bx_dt):
+def check_bxes_dict(bx_dt):
     """ checks if all boxes are unlocked """
     for box in bx_dt:
         if not bx_dt[box]:
