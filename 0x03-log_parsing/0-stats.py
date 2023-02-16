57d8294de3e90cfe6c1df776c51fbb44ad972dab
vantwill <vtwill41@gmail>
2023-02-16 22:25:09 +0300

    commit

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,46 +1,40 @@
 #!/usr/bin/python3
-""" parses logs """
+"""Performs log parsing from stdin"""
 
-
+import re
 import sys
+counter = 0
+file_size = 0
+statusC_counter = {200: 0, 301: 0, 400: 0,
+                   401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
 
-def main():
-    """ main func """
-    total_file_size = 0
-    status_codes_count_map = {"200": 0, "301": 0, "400": 0, "401": 0,
-                              "403": 0, "404": 0, "405": 0, "500": 0}
-    try:
-        count = 0
-        for line in sys.stdin:
-            count += 1
-            tokens = line.split()
-            if len(tokens):
-                try:
-                    total_file_size += int(tokens[-1])
-                    status_codes_count_map[tokens[-2]] += 1
-                except Exception:
-                    pass
-                if count == 10:
-                    count = 0
-                    print_report(
-                        status_codes_count_map,
-                        total_file_size
-                    )
-        print_report(status_codes_count_map, total_file_size)
-
-    except KeyboardInterrupt:
-        print_report(status_codes_count_map, total_file_size)
-        raise
-
-
-def print_report(dct_, file_size):
-    """ prints to stdout summary of logs """
-    print("File size: {}".format(file_size))
-    for key in sorted(dct_.keys()):
-        if dct_.get(key):
-            print("{}: {}".format(key, dct_[key]))
+def printCodes(dict, file_s):
+    """Prints the status code and the number of times they appear"""
+    print("File size: {}".format(file_s))
+    for key in sorted(dict.keys()):
+        if statusC_counter[key] != 0:
+            print("{}: {}".format(key, dict[key]))
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        for line in sys.stdin:
+            split_string = re.split('- |"|"| " " ', str(line))
+            statusC_and_file_s = split_string[-1]
+            if counter != 0 and counter % 10 == 0:
+                printCodes(statusC_counter, file_size)
+            counter = counter + 1
+            try:
+                statusC = int(statusC_and_file_s.split()[0])
+                f_size = int(statusC_and_file_s.split()[1])
+                # print("Status Code {} size {}".format(statusC, f_size))
+                if statusC in statusC_counter:
+                    statusC_counter[statusC] += 1
+                file_size = file_size + f_size
+            except:
+                pass
+        printCodes(statusC_counter, file_size)
+    except KeyboardInterrupt:
+        printCodes(statusC_counter, file_size)
+        raise
